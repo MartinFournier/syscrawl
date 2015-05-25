f937dd46d67cdebfd501b6a08fe8e62c3c408ace
Martin Fournier <fournier.mart@gmail.com>
2015-05-25 14:58:16 -0400

    Change StrangeIoC version to point to master + switch to copy files instead of symlink

--- configs/install.sh
+++ configs/install.sh
@@ -4,4 +4,6 @@ cd configs
 bower install
 nuget install -OutputDirectory ../packages/
 cd ..
+rm -r unity/Assets/Packages/StrangeIoC
+cp -r packages/strangeioc/StrangeIoC/scripts/strange/ unity/Assets/Packages/StrangeIoC
 echo "Done"
