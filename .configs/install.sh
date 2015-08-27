7f5153f1c45199641549344a381b6dff0045ffc4
Martin Fournier <fournier.mart@gmail.com>
2015-08-27 13:12:35 -0400

    Cleanup install script to add details

--- .configs/install.sh
+++ .configs/install.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 #chmod u+rwx configs/install.sh
 
+#Folders
 CONFIG_FOLDER=".configs"
 PACKAGES_FOLDER=".3rd"
-PACKAGES_OUTPUT="Assets/Packages"
-
+PACKAGES_OUTPUT="Assets/Vendor"
+SEP="########################"
+echo "Fetching the packages from bower & nuget"
+echo $SEP
+#Fetch the packages
 cd $CONFIG_FOLDER
 bower install
 nuget install -OutputDirectory ../$PACKAGES_FOLDER/
 cd ..
 
+echo "Cleanup"
+echo $SEP
+#Cleanup & Prep
 rm -r $PACKAGES_OUTPUT
 mkdir -p $PACKAGES_OUTPUT
+
+echo "Copying packages"
+echo $SEP
+#StrangeIoC (only the framework, excluding examples)
 cp -r $PACKAGES_FOLDER/strangeioc/StrangeIoC/scripts/strange/ $PACKAGES_OUTPUT/StrangeIoC
+# NUnit, NGenerics
 cp -r $PACKAGES_FOLDER/N* $PACKAGES_OUTPUT/
 
-echo "Done"
+echo ""
+echo "Install Complete."
