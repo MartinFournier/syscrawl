3b634e77cb23197c8f03debd44ed65a87522aec1
Martin Fournier <fournier.mart@gmail.com>
2015-08-27 12:48:50 -0400

    Update project version

--- configs/install.sh
+++ configs/install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 #chmod u+rwx configs/install.sh
+PROJECT="syscrawl"
+
 cd configs
 bower install
 nuget install -OutputDirectory ../packages/
 cd ..
-rm -r unity/Assets/Packages/StrangeIoC
-cp -r packages/strangeioc/StrangeIoC/scripts/strange/ unity/Assets/Packages/StrangeIoC
+
+mkdir -p $PROJECT/Assets/Packages/StrangeIoC
+rm -r $PROJECT/Assets/Packages/StrangeIoC
+cp -r packages/strangeioc/StrangeIoC/scripts/strange/ $PROJECT/Assets/Packages/StrangeIoC
+
 echo "Done"
